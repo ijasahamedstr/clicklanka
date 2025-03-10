2d32413258630ead7bc127f14f8809e99dd0e442
ijasahamedstr <ijasahamed.str@gmail.com>
2025-03-10 13:24:45 +0200

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
